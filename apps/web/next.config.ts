6634402a31be6b37ddf435e1057e950237761613
fashshiddi88 <fajrishidi8@gmail.com>
2025-07-05 20:25:49 +0700

    fix: nextconfig2

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,15 +1,57 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['res.cloudinary.com'],
+  compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'twemoji.maxcdn.com',
+      },
+    ],
+  },
+
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
+  },
+
+  outputFileTracingIncludes: {
+    '/api/users/cv/generate': ['../../node_modules/@sparticuz/chromium/bin/**'],
+    '/api/users/certificates/**': [
+      '../../node_modules/@sparticuz/chromium/bin/**',
+    ],
   },
 };
 
